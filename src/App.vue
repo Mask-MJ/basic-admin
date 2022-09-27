538b138ff19288961f2641dd0c065dbf45dc92a8
moyaojun <617473294@qq.com>
2022-09-27 14:48:37 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -13,5 +7,4 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
