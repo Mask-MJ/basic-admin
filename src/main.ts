7d50a0922f6d415be68659e2b562fd2cd27d19ca
moyaojun <617473294@qq.com>
2022-09-27 13:06:46 +0800

    feat: router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
+import 'virtual:windi-base.css';
+import 'virtual:windi-components.css';
+import 'virtual:windi-utilities.css';
+
 import { createApp } from 'vue';
 import App from './App.vue';
+import { setupRouter } from '/@/router';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+// 配置路由
+setupRouter(app);
+app.mount('#app');
