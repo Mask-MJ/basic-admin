01026125b456c43616509238ca4ed8a129e41e26
moyaojun <617473294@qq.com>
2022-09-29 13:11:57 +0800

    feat: layout menu

---
+++ src/layouts/default/sider/AppLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="anticon" @click="goHome">
+    <img class="w-8 h-8" src="../../../assets/images/logo.png" />
+    <div class="ml-2 truncate md:opacity-100"> NGEVmate </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const goHome = () => {};
+</script>
+
+<style lang="less" scoped>
+  .anticon {
+    // display: flex;
+    // align-items: center;
+    // padding-left: 7px;
+    // cursor: pointer;
+    // transition: all 0.2s ease;
+    // height: 48px;
+    // padding: 10px 4px 10px 10px;
+    // color: #fff;
+    // font-weight: 700;
+    // font-size: 16px;
+  }
+</style>
